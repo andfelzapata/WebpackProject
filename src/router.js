ff940aeac00bfb927a7f843022b25ce43095ad30
Andres Zapata <andreszapatacano@gmail.com>
2018-01-29 23:46:04 -0500

    Refactor router.js to use react-router v4

--- src/router.js
+++ src/router.js
@@ -1,21 +1,18 @@
 import React from 'react';
-import { Router, Route, IndexRoute, hashHistory } from 'react-router';
-
-import Home from './components/Home';
-import ArtistMain from './components/artists/ArtistMain';
-import ArtistDetail from './components/artists/ArtistDetail';
-import ArtistCreate from './components/artists/ArtistCreate';
-import ArtistEdit from './components/artists/ArtistEdit';
+import {
+  Route,
+  BrowserRouter as Router
+} from 'react-router-dom';
+import Home from 'components/Home';
+import Header from 'components/Header';
 
 const Routes = () => {
   return (
-    <Router history={hashHistory}>
-      <Route path="/" component={Home}>
-        <IndexRoute component={ArtistMain} />
-        <Route path="artists/new" component={ArtistCreate} />
-        <Route path="artists/:id" component={ArtistDetail} />
-        <Route path="artists/:id/edit" component={ArtistEdit} />
-      </Route>
+    <Router>
+      <div>
+        <Header />
+        <Route path="/" component={Home} />
+      </div> c
     </Router>
   );
 };
