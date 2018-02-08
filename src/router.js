ee794649a74cb585ad0fee2d534b9b5acfc2b138
Andres Zapata <andreszapatacano@gmail.com>
2018-02-07 23:02:05 -0500

    User browser router

--- src/router.js
+++ src/router.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {
   Route,
-  HashRouter as Router,
+  BrowserRouter as Router,
 } from 'react-router-dom';
 import Header from 'components/Header';
 import ArtistMain from 'components/artists/ArtistMain';
