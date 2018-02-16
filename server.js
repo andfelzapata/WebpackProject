028e849411c776d047c8f83801d0de1e6bce08f8
Andres Zapata <andreszapatacano@gmail.com>
2018-02-15 22:26:38 -0500

    Add deploy:node script

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 const express = require('express')
-const webpackMiddleWare = require('webpack-dev-middleware')
-const webpack = require('webpack')
-const webpackConfig = require('./webpack.config.js')
+const path = require('path')
 
 const app = express()
 
-app.use(webpackMiddleWare(webpack(webpackConfig)))
+if(process.env.NODE_ENV !== 'production') {
+  const webpack = require('webpack')
+  const webpackConfig = require('./webpack.config.js')
+  const webpackMiddleWare = require('webpack-dev-middleware')
+
+  app.use(webpackMiddleWare(webpack(webpackConfig)))
+} else {
+  app.use(express.static('dist'))
+  app.get('*',(req, res) => {
+    res.sendFile(path.join(__dirname, 'dist/index.html'))
+  })
+}
 
 app.listen(3050, () => console.log('Listening'))
